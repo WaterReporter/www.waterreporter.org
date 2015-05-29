895ac2dbcc69041a2ea3eb7930d4d9038698eae3
Joshua Powell <joshua@viable.io>
2015-05-29 11:58:42 -0400

    Added new Submit module;

---
+++ app/modules/components/submit/submit--routes.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name WaterReporter.submit.config:submitReport-routes
+ * @description
+ * # Water Reporter App
+ *
+ * Routes for the states applying to the submit report page of the application.
+ */
+
+angular.module('WaterReporter.submit')
+  .config(function ($stateProvider) {
+    
+    $stateProvider
+      .state('submit', {
+        url: '/submit',
+        templateUrl: '/modules/components/submit-report/submitReport-view.html',
+        controller: 'SubmitReportController',
+        controllerAs: 'submitReport'
+      });
+      
+  });
