7b67cbb55b75bacca7a358352a30d416845bc95f
Joshua Powell <joshua@viable.io>
2015-06-08 14:48:44 -0400

    Added new filter to the Activity Feed page Report query to ensure that reports are being sorted properly;

--- app/modules/components/activity/activity--routes.js
+++ app/modules/components/activity/activity--routes.js
@@ -16,8 +16,34 @@ angular.module('WaterReporter')
         controllerAs: 'activity',
         reloadOnSearch: false,
         resolve: {
-          reports: function($location, $route, Report) { 
-            return Report.query($location.search());
+          reports: function($location, $route, Report) {
+
+            //
+            // Get all of our existing URL Parameters so that we can
+            // modify them to meet our goals
+            //
+            var search_params = $location.search();
+
+            //
+            // Prepare any pre-filters to append to any of our user-defined
+            // filters in the browser address bar
+            //
+            if (search_params.q) {
+              search_params.q = angular.fromJson(search_params.q);
+
+              search_params.q.filters = (search_params.q.filters) ? search_params.q.filters : [];
+              search_params.q.order_by = (search_params.q.order_by) ? search_params.q.order_by : [];
+
+              //
+              // Ensure that returned Report features are sorted newest first
+              //
+              search_params.q.order_by.push({
+                field: 'report_date',
+                direction: 'desc'
+              });
+            }
+
+            return Report.query(search_params);
           }
         }            
       });
