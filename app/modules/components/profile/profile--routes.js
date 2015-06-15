7a05ec6a88ddf3ada3d8c5116eed37aed215a27f
Joshua Powell <joshua@viable.io>
2015-06-15 16:17:34 -0400

    Included the new Profiles Controller; Added new resolve for profiles lookup; Added new Profiles easy url in header;

--- app/modules/components/profile/profile--routes.js
+++ app/modules/components/profile/profile--routes.js
@@ -12,14 +12,38 @@
 angular.module('WaterReporter')
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/profile', {
-        templateUrl: '/modules/components/profile/profile--view.html',
-        controller: 'ProfileController',
-        controllerAs: 'profile'
-      })
-      .when('/profile/:userId', {
+      .when('/profiles', {
         templateUrl: '/modules/components/profile/profiles--view.html',
         controller: 'ProfilesController',
+        controllerAs: 'profiles',
+        resolve: {
+          profiles: function($location, User) {
+
+            //
+            // Get all of our existing URL Parameters so that we can
+            // modify them to meet our goals
+            //
+            var search_params = $location.search();
+
+            //
+            // Prepare any pre-filters to append to any of our user-defined
+            // filters in the browser address bar
+            //
+            search_params.q = (search_params.q) ? angular.fromJson(search_params.q) : {};
+
+            search_params.q.filters = (search_params.q.filters) ? search_params.q.filters : [];
+            search_params.q.order_by = (search_params.q.order_by) ? search_params.q.order_by : [];
+
+            //
+            // Execute our query so that we can get the Reports back
+            //
+            return User.query(search_params);
+          }
+        }
+      })
+      .when('/profiles/:userId', {
+        templateUrl: '/modules/components/profile/profile--view.html',
+        controller: 'ProfileController',
         controllerAs: 'profile'
       });
   });
