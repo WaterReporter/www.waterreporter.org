68a6311e4f14b913c157f506bd39c4768d0c8f9e
Joshua Powell <joshua@viable.io>
2015-06-15 16:14:34 -0400

    added new profile routes;

--- app/modules/components/profile/profile--routes.js
+++ app/modules/components/profile/profile--routes.js
@@ -21,5 +21,5 @@ angular.module('WaterReporter')
         templateUrl: '/modules/components/profile/profiles--view.html',
         controller: 'ProfilesController',
         controllerAs: 'profile'
-      });      
+      });
   });
