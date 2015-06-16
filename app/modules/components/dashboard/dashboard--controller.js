68d6aca1f570d1df387404429b78f285830e04e7
Joshua Powell <joshua@viable.io>
2015-06-16 17:03:22 -0400

    Added new dashboard module;

---
+++ app/modules/components/dashboard/dashboard--controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name WaterReporter.report.controller:SingleReportController
+ * @description
+ *     Display a single report based on the current `id` provided in the URL
+ * Controller of the waterReporterApp
+ */
+angular.module('WaterReporter')
+  .controller('DashboardController', function () {
+
+    var self = this;
+
+  });
---
+++ app/modules/components/dashboard/dashboard--routes.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name WaterReporter.singleReport.config:report-routes
+ * @description
+ * # Water Reporter App
+ *
+ * Routes for the states applying to the single report page of the application.
+ */
+
+angular.module('WaterReporter')
+  .config(function ($routeProvider) {
+    
+    $routeProvider
+      .when('/dashboard', {
+        templateUrl: '/modules/components/dashboard/dashboard--view.html',
+        controller: 'DashboardController',
+        controllerAs: 'dashbaord',
+        resolve: {
+        }
+      });
+
+  });
