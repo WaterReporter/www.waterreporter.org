23a1859e63abb6693b87691789432a43a92b458a
Joshua Powell <joshua@viable.io>
2015-06-17 15:50:00 -0400

    Added Account service;

---
+++ app/modules/shared/api/account--service.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name WaterReporter
+ * @description
+ * Provides access to the User endpoint of the WaterReporter API
+ * Service in the WaterReporter.
+ */
+angular.module('WaterReporter')
+  .service('Account', function (ipCookie) {
+    
+    var Account = {
+
+    };
+
+    return Account;
+  });
