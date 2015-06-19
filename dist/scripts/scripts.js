ad0e15533e2a30aad353bbc2d8207344408aeedd
Joshua Powell <joshua@viable.io>
2015-06-19 10:39:39 -0400

    Release build featuring geography display on Activity Feed and the addition of a basic dashboard;

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -1180,10 +1180,6 @@ angular.module('WaterReporter')
         //
         config.params = (config.params === undefined) ? {} : config.params;
 
-        if (licenseeCookie) {
-          config.params.licensee = licenseeCookie;
-        }
-
         console.debug('SecurityInterceptor::Request', config || $q.when(config));
 
         return config || $q.when(config);
@@ -1437,8 +1433,42 @@ angular.module('WaterReporter')
       .when('/dashboard', {
         templateUrl: '/modules/components/dashboard/dashboard--view.html',
         controller: 'DashboardController',
-        controllerAs: 'dashbaord',
+        controllerAs: 'dashboard',
         resolve: {
+          reports: function($location, $route, Report) {
+
+            //
+            // Get all of our existing URL Parameters so that we can
+            // modify them to meet our goals
+            //
+            var search_params = $location.search();
+
+            //
+            // Prepare any pre-filters to append to any of our user-defined
+            // filters in the browser address bar
+            //
+            search_params.q = (search_params.q) ? angular.fromJson(search_params.q) : {};
+
+            search_params.q.filters = (search_params.q.filters) ? search_params.q.filters : [];
+            search_params.q.order_by = (search_params.q.order_by) ? search_params.q.order_by : [];
+
+            //
+            // We need to add the current user's territory to this 
+            //
+
+            //
+            // Ensure that returned Report features are sorted newest first
+            //
+            search_params.q.order_by.push({
+              field: 'report_date',
+              direction: 'desc'
+            });
+
+            //
+            // Execute our query so that we can get the Reports back
+            //
+            return Report.query(search_params);
+          }
         }
       });
 
