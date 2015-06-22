9c3a222ca92837230f0c396faca0243d61df4c25
Joshua Powell <joshua@viable.io>
2015-06-22 11:09:40 -0400

    Release build featuring enahnced user account handling;

--- dist/scripts/scripts.7e2a25cc.js
+++ dist/scripts/scripts.7e2a25cc.js
@@ -1260,7 +1260,7 @@ angular.module('WaterReporter')
  * Service in the WaterReporter.
  */
 angular.module('WaterReporter')
-  .factory('SecurityInterceptor', ['$q', 'ipCookie', function ($q, ipCookie) {
+  .factory('SecurityInterceptor', function ($q, ipCookie, $location) {
 
     return {
       request: function(config) {
@@ -1298,10 +1298,15 @@ angular.module('WaterReporter')
       },
       responseError: function (response) {
         console.debug('AuthorizationInterceptor::ResponseError', response || $q.when(response));
+
+        if (response.status === 403) {
+          $location.path('/user/logout');
+        }
+
         return $q.reject(response);
       }
     };
-  }]).config(function ($httpProvider) {
+  }).config(function ($httpProvider) {
     $httpProvider.interceptors.push('SecurityInterceptor');
   });
 
