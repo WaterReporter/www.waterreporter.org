42977b79d777ea11346d81513e1dd64a6c62e9e1
Joshua Powell <joshua@viable.io>
2015-06-22 08:53:39 -0400

    Added variable login information to the activity controller and revised how the header handles these requests;

--- app/modules/components/activity/activity--controller.js
+++ app/modules/components/activity/activity--controller.js
@@ -43,4 +43,17 @@ angular.module('WaterReporter')
 
     this.search.data = reports;
 
+
+    /**
+     * This is the first page the authneticated user will see. We need to make
+     * sure that their user information is ready to use. Make sure the
+     * Account.userObject contains the appropriate information.
+     */
+    if (Account.userObject && !Account.userObject.id) {
+      user.$promise.then(function(userResponse) {
+        Account.userObject = userResponse;
+          $rootScope.user = Account.userObject;
+      });
+    }
+
   });
