e98d88505d915296482350bb2b898f832cbf9c28
Joshua Powell <joshua@viable.io>
2015-07-17 09:55:21 -0400

    Created a separate keyword search page;

---
+++ app/modules/components/search/search--controller.js
@@ -0,0 +1,64 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name
+ * @description
+ */
+angular.module('WaterReporter')
+  .controller('SearchController', function (Account, Report, reports, $rootScope, Search, user) {
+
+    var self = this;
+
+    $rootScope.user = Account.userObject;
+
+    /**
+     * Setup search capabilities for the Report Activity Feed
+     *
+     * @data this.search
+     *    loads the Search Service into our page scope
+     * @data this.search.params
+     *    loads the default url parameters into the page fields
+     * @data this.search.model
+     *    tells the Search Service what the data model for this particular search looks like
+     * @data this.search.resource
+     *    tells the Search Service what resource to perform the search with
+     * @data this.search.data
+     *    retains and updates based on the features returned from the user-defined query
+     *
+     */
+    this.search = Search;
+
+    this.search.params = Search.defaults();
+
+    this.search.model = {
+      report_description: {
+        name: 'report_description',
+        op: 'ilike',
+        val: ''
+      }
+    };
+
+    this.search.resource = Report;
+
+    this.search.data = reports;
+
+
+    /**
+     * This is the first page the authneticated user will see. We need to make
+     * sure that their user information is ready to use. Make sure the
+     * Account.userObject contains the appropriate information.
+     */
+    if (Account.userObject && !Account.userObject.id) {
+      if (user) {
+        user.$promise.then(function(userResponse) {
+          Account.userObject = userResponse;
+            $rootScope.user = Account.userObject;
+
+            $rootScope.isLoggedIn = Account.hasToken();
+            $rootScope.isAdmin = Account.hasRole('admin');
+        });
+      }
+    }
+
+  });
---
+++ app/modules/components/search/search--routes.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name
+ * @description
+ */
+
+angular.module('WaterReporter')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/search', {
+        templateUrl: '/modules/components/search/search--view.html',
+        controller: 'SearchController',
+        controllerAs: 'search',
+        reloadOnSearch: false,
+        resolve: {
+          reports: function($location, $route, Report) {
+
+            //
+            // Get all of our existing URL Parameters so that we can
+            // modify them to meet our goals
+            //
+            var search_params = $location.search();
+
+            //
+            // Prepare any pre-filters to append to any of our user-defined
+            // filters in the browser address bar
+            //
+            search_params.q = (search_params.q) ? angular.fromJson(search_params.q) : {};
+
+            search_params.q.filters = (search_params.q.filters) ? search_params.q.filters : [];
+            search_params.q.order_by = (search_params.q.order_by) ? search_params.q.order_by : [];
+
+            //
+            // Ensure that returned Report features are sorted newest first
+            //
+            search_params.q.order_by.push({
+              field: 'report_date',
+              direction: 'desc'
+            });
+
+            //
+            // Execute our query so that we can get the Reports back
+            //
+            return Report.query(search_params);
+          },
+          user: function(Account) {
+            return (Account.userObject && !Account.userObject.id) ? Account.getUser() : Account.userObject;
+          }
+        }
+      });
+  });
