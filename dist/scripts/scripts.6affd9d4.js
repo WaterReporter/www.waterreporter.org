b5ea1028f967136e21308cd098530000cac30de1
Joshua Powell <joshua@viable.io>
2015-08-20 16:17:44 -0400

    Release build featuring an enhanced Report page

--- dist/scripts/scripts.6affd9d4.js
+++ dist/scripts/scripts.6affd9d4.js
@@ -3150,8 +3150,6 @@ angular.module('WaterReporter')
      */
     this.map = Map;
 
-    L.Icon.Default.imagePath = '/images';
-
     report.$promise.then(function(reportResponse) {
 
       self.report = reportResponse;
