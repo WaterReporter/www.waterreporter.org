2f72aeb9c4e162ebe590b047393c1f9e7da92151
Joshua Powell <joshua@viable.io>
2015-08-20 12:36:54 -0400

    Release build that fixes #110

--- dist/scripts/scripts.e6662461.js
+++ dist/scripts/scripts.e6662461.js
@@ -2243,7 +2243,7 @@ angular.module('WaterReporter')
    * @description
    */
   angular.module('WaterReporter')
-    .controller('ActivityController', function (Account, features, $location, leafletData, leafletEvents, Map, mapbox, mapboxGeometry, Report, reports, $rootScope, $scope, Search, user) {
+    .controller('ActivityController', function (Account, features, $location, leafletData, leafletEvents, Map, mapbox, mapboxGeometry, Report, reports, $rootScope, $scope, user) {
 
       var self = this;
 
@@ -2257,24 +2257,7 @@ angular.module('WaterReporter')
 
       this.permissions = {};
 
-      /**
-       * Setup search capabilities for the Report Activity Feed
-       *
-       * @data this.search
-       *    loads the Search Service into our page scope
-       * @data this.search.params
-       *    loads the default url parameters into the page fields
-       * @data this.search.model
-       *    tells the Search Service what the data model for this particular search looks like
-       * @data this.search.resource
-       *    tells the Search Service what resource to perform the search with
-       * @data this.search.data
-       *    retains and updates based on the features returned from the user-defined query
-       *
-       */
-      this.search = Search;
-
-      this.search.data = reports;
+      this.data = reports;
 
       //
       // This is the first page the authneticated user will see. We need to make
@@ -2468,54 +2451,22 @@ angular.module('WaterReporter')
  * Controller of the waterReporterApp
  */
 angular.module('WaterReporter')
-  .controller('HomeController', function (Account, Report, $rootScope, Search, user) {
+  .controller('HomeController', function (Account, $location, Report, $rootScope, user) {
 
     var self = this;
 
-    /**
-     * Setup search capabilities for the Report Activity Feed
-     *
-     * @data this.search
-     *    loads the Search Service into our page scope
-     * @data this.search.params
-     *    loads the default url parameters into the page fields
-     * @data this.search.model
-     *    tells the Search Service what the data model for this particular search looks like
-     * @data this.search.resource
-     *    tells the Search Service what resource to perform the search with
-     * @data this.search.data
-     *    retains and updates based on the features returned from the user-defined query
-     *
-     */
-    this.search = Search;
+    //
+    // This is the first page the authneticated user will see. We need to make
+    // sure that their user information is ready to use. Make sure the
+    // Account.userObject contains the appropriate information.
+    //
+    if (Account.userObject && user) {
+      user.$promise.then(function(userResponse) {
+        $rootScope.user = Account.userObject = userResponse;
 
-    this.search.model = {
-      report_description: {
-        name: 'report_description',
-        op: 'ilike',
-        val: ''
-      }
-    };
+        $location.path('/activity');
 
-    this.search.resource = Report;
-
-    /**
-     * This is the first page the authneticated user will see. We need to make
-     * sure that their user information is ready to use. Make sure the
-     * Account.userObject contains the appropriate information.
-     */
-    if (Account.userObject && !Account.userObject.id) {
-      if (user) {
-        user.$promise.then(function(userResponse) {
-          $rootScope.user = Account.userObject = userResponse;
-
-          self.permissions = {
-            isLoggedIn: Account.hasToken(),
-            isAdmin: Account.hasRole('admin')
-          };
-
-        });
-      }
+      });
     }
 
   });
