8dbb763de5893ca67ba17cf486204be038350290
Joshua Powell <joshua@viable.io>
2015-09-01 16:17:13 -0400

    Added moving slideshow to the home page;

--- app/modules/components/pages/pages--controller.js
+++ app/modules/components/pages/pages--controller.js
@@ -8,9 +8,55 @@
  * Controller of the waterReporterApp
  */
 angular.module('WaterReporter')
-  .controller('PageController', function (Account, $location, Notifications, $rootScope, user) {
+  .controller('PageController', function (Account, $location, Notifications, $rootScope, $interval, user) {
 
     var self = this;
+
+    self.slides = {
+      data: [
+        {
+          url: '/images/slide_1.jpg',
+          title: 'Use the satellite or trails map to explore other peoples reports'
+        },
+        {
+          url: '/images/slide_2.jpg'
+        },
+        // {
+        //   url: '/images/slide_3.jpg'
+        // },
+        {
+          url: '/images/slide_4.jpg'
+        },
+        {
+          url: '/images/slide_5.jpg'
+        },
+        {
+          url: '/images/slide_6.jpg'
+        },
+        {
+          url: '/images/slide_7.jpg'
+        },
+        {
+          url: '/images/slide_8.jpg'
+        },
+        {
+          url: '/images/slide_9.jpg'
+        },
+        {
+          url: '/images/slide_10.jpg'
+        }
+      ],
+      visible: 0
+    };
+
+    $interval(function() {
+      if ((self.slides.data.length - 1) > self.slides.visible) {
+        self.slides.visible = self.slides.visible + 1;
+      }
+      else if ((self.slides.data.length - 1) <= self.slides.visible) {
+        self.slides.visible = 0;
+      }
+    }, 3500);
 
     //
     // This is the first page the authneticated user will see. We need to make
