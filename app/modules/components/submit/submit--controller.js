a768da6e6c00d4018d3227be4a7f7b3ce904cc57
Joshua Powell <joshua@viable.io>
2015-09-02 12:00:28 -0400

    Fixes #151 by implementing an alternative form validation process;

--- app/modules/components/submit/submit--controller.js
+++ app/modules/components/submit/submit--controller.js
@@ -186,6 +186,14 @@
       //
       self.save = function() {
 
+        if (!self.report.report_description) {
+          $rootScope.notifications.warning('Un-oh!', 'You forgot to add a comment to your report');
+          return false;
+        } else if (!self.image) {
+          $rootScope.notifications.warning('Un-oh!', 'You forgot to add a photo to your report');
+          return false;
+        }
+
         self.status.saving.action = true;
         self.status.saving.message = 'Uploading your image...';
 
