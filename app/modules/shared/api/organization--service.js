35ce5a42e43d0931bf9d7ba4d290372fa7921ed6
Joshua Powell <joshua@viable.io>
2015-11-20 08:45:20 -0500

    Used Function method in Organization service;

--- app/modules/shared/api/organization--service.js
+++ app/modules/shared/api/organization--service.js
@@ -1,20 +1,24 @@
-'use strict';
+(function() {
 
-/**
- * @ngdoc service
- * @name WaterReporter
- * @description
- * Provides access to the Organization endpoint of the WaterReporter API
- * Service in the WaterReporter.
- */
-angular.module('WaterReporter')
-  .service('Organization', function ($resource) {
-    return $resource('//api.waterreporter.org/v1/data/organization/:organizationId', {}, {
-      query: {
-        isArray: false
-      },
-      update: {
-        method: 'PATCH'
-      }
+  'use strict';
+
+  /**
+   * @ngdoc service
+   * @name WaterReporter
+   * @description
+   * Provides access to the Organization endpoint of the WaterReporter API
+   * Service in the WaterReporter.
+   */
+  angular.module('WaterReporter')
+    .service('Organization', function ($resource) {
+      return $resource('//api.waterreporter.org/v1/data/organization/:organizationId', {}, {
+        query: {
+          isArray: false
+        },
+        update: {
+          method: 'PATCH'
+        }
+      });
     });
-  });
+
+}());
