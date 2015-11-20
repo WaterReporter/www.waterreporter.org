a6d0e810b1dd517df7b176b78452f706b5dc4427
Joshua Powell <joshua@viable.io>
2015-11-20 08:56:00 -0500

    Added stubbed out Organization profile

---
+++ app/modules/components/organization/organizationList--controller.js
@@ -0,0 +1,17 @@
+(function() {
+
+  'use strict';
+
+  /**
+   * @ngdoc function
+   * @name WaterReporter.submit.controller:SubmitController
+   * @description
+   * # SubmitReportController
+   * Controller of the waterReporterApp
+   */
+  angular.module('WaterReporter')
+    .controller('OrganizationListController', function (Organization) {
+
+    });
+
+}());
