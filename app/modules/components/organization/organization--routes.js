b14b8eb0b4cc268259b33cb667fb8f4326d194d3
Joshua Powell <joshua@viable.io>
2015-11-20 09:11:29 -0500

    Added basic organization listing to the Organization List page; Added search functionality to the Organization list page; Tested and verfied basic pagination works for Organization page;

--- app/modules/components/organization/organization--routes.js
+++ app/modules/components/organization/organization--routes.js
@@ -11,11 +11,27 @@ angular.module('WaterReporter')
   .config(function ($routeProvider) {
     $routeProvider
       .when('/organizations', {
-        templateUrl: '/modules/components/organization/organization--view.html',
+        templateUrl: '/modules/components/organization/views/organizationList--view.html',
         controller: 'OrganizationListController',
         controllerAs: 'page',
         reloadOnSearch: false,
         resolve: {
+          organizations: function(Organization) {
+            //
+            // Execute our query so that we can get the Reports back
+            //
+            return Organization.query({
+              q: {
+                order_by: [
+                  {
+                    field: 'name',
+                    direction: 'asc'
+                  }
+                ]
+              },
+              page: 1
+            });
+          },
           user: function(Account) {
             return (Account.userObject && !Account.userObject.id) ? Account.getUser() : Account.userObject;
           }
