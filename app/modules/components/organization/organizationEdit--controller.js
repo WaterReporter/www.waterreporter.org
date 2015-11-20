9f319a62467aef357d544430ad185d1a1cc9fb0b
Joshua Powell <joshua@viable.io>
2015-11-20 09:00:09 -0500

    Added organization Edit and Profile controllers and views; Added organization list view

---
+++ app/modules/components/organization/organizationEdit--controller.js
@@ -0,0 +1,15 @@
+(function() {
+
+  'use strict';
+
+  /**
+   * @ngdoc function
+   * @name WaterReporter.submit.controller:OrganizationEditController
+   * @description
+   */
+  angular.module('WaterReporter')
+    .controller('OrganizationEditController', function (Organization) {
+
+    });
+
+}());
---
+++ app/modules/components/organization/organizationProfile--controller.js
@@ -0,0 +1,15 @@
+(function() {
+
+  'use strict';
+
+  /**
+   * @ngdoc function
+   * @name WaterReporter.submit.controller:OrganizationProfileController
+   * @description
+   */
+  angular.module('WaterReporter')
+    .controller('OrganizationProfileController', function (Organization) {
+
+    });
+
+}());
