33ca12343daa9251964853210d9393f3908a651e
Joshua Powell <joshua@viable.io>
2015-12-14 14:16:50 -0500

    Fixed syntax error in the User API Service

--- app/modules/config/environment.js
+++ app/modules/config/environment.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {name:'development',apiEndpoint:'http://127.0.0.1:5000'})
+.constant('environment', {name:'development',apiUrl:'http://127.0.0.1:5000'})
 
 ;
--- app/modules/shared/api/user--service.js
+++ app/modules/shared/api/user--service.js
@@ -1,36 +1,40 @@
-'use strict';
+(function() {
 
-/**
- * @ngdoc service
- * @name WaterReporter
- * @description
- * Provides access to the User endpoint of the WaterReporter API
- * Service in the WaterReporter.
- */
-angular.module('WaterReporter')
-  .service('User', ['$resource', function (environment, $resource) {
-    return $resource(environment.apiUrl.concat('/v1/data/user/:id'), {
-      id: '@id'
-    }, {
-      query: {
-        isArray: false
-      },
-      update: {
-        method: 'PATCH'
-      },
-      getOrganizations: {
-        method: 'GET',
-        isArray: false,
-        url: environment.apiUrl.concat('/v1/data/user/:id/organization')
-      },
-      me: {
-        method: 'GET',
-        url: environment.apiUrl.concat('/v1/data/me')
-      },
-      classifications: {
-        method: 'GET',
-        isArray: false,
-        url: environment.apiUrl.concat('/v1/data/user/:id/classifications')
-      }
+  'use strict';
+
+  /**
+   * @ngdoc service
+   * @name WaterReporter
+   * @description
+   * Provides access to the User endpoint of the WaterReporter API
+   * Service in the WaterReporter.
+   */
+  angular.module('WaterReporter')
+    .service('User', function (environment, $resource) {
+      return $resource(environment.apiUrl.concat('/v1/data/user/:id'), {
+        id: '@id'
+      }, {
+        query: {
+          isArray: false
+        },
+        update: {
+          method: 'PATCH'
+        },
+        getOrganizations: {
+          method: 'GET',
+          isArray: false,
+          url: environment.apiUrl.concat('/v1/data/user/:id/organization')
+        },
+        me: {
+          method: 'GET',
+          url: environment.apiUrl.concat('/v1/data/me')
+        },
+        classifications: {
+          method: 'GET',
+          isArray: false,
+          url: environment.apiUrl.concat('/v1/data/user/:id/classifications')
+        }
+      });
     });
-  }]);
+
+}());
