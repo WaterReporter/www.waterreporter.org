3158977f3e0c96a1eedd566a176e5e3ade8d4e4e
Joshua Powell <joshua@viable.io>
2016-01-04 14:11:52 -0500

    Added the Groups module;

---
+++ app/modules/components/groups/groups--service.js
@@ -0,0 +1,19 @@
+(function() {
+
+  'use strict';
+
+  /**
+   * @ngdoc service
+   * @name User Groups
+   * @description
+   * Provides access to functionality necessary for anonymous and authenticated
+   * users to interact with the User Groups functionality.
+   */
+   angular.module('WaterReporter')
+    .service('UserGroups', function(){
+      return {
+
+      };
+    });
+
+}());
